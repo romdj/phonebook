2e98862af836f8064ad5bc9d215c4689dc552ee0
Romain Lussier <romain.lussier@external.engie.com>
2018-05-13 12:26:26 +0200

    added model to app CR done UD remaining

--- Application/js/phonebookController.js
+++ Application/js/phonebookController.js
@@ -4,20 +4,41 @@ myAppModule.controller('phonebookController', function ($scope, $http, $location
 
   function init() {
     console.log('hello world!');
+    $scope.fetchData();
   }
 
-  $scope.data = [{
-      name: "Tom",
-      tel: "012-345-6789"
-    },
-    {
-      name: "Jerry",
-      tel: "012-345-6789"
-    },
-    {
-      name: "Philip",
-      tel: "012-345-6789"
-    }
-  ];
+  $scope.fetchData = function () {
+    $http.get("/api/phonebooks")
+      .success(function (response) {
+        $scope.data = response;
+      })
+      .error(function (error) {
+        console.log("error Fetching Data : " + error);
+      });
+    setTimeout($scope.fetchData, 2000);
+  }
+
+  $scope.addContact = function(newName,newTel){
+    $scope.data.push({name:newName,tel:newTel});
+    $http.post("/api/phonebook",JSON.stringify({name:newName,tel:newTel}),{headers:{'content-type':'application/json'}})
+      .success(function (response) {
+        console.log('successfully added new Contact');
+      })
+      .error(function (error) {
+        console.log("error adding new Contact : " + error);
+      });
+  }
+  
+  $scope.deleteContact = function(contactId){
+    console.log('inside deleteContact');
+    console.log('Contacts:', $scope.data);
+    $http.delete("/api/phonebook/" + contactId)
+      .success(function (response) {
+        console.log('successfully removed Contact: ', contactId);
+      })
+      .error(function (error) {
+        console.log("error adding new Contact : " + error);
+      });
+  }
   init();
 });
--- expressserver.js
+++ expressserver.js
@@ -1,7 +1,70 @@
-var express = require('express');
-var app = express();
-var path = require('path');
+const express = require('express');
+const app = express();
+const path = require('path');
+const mongoose = require('mongoose');
+const Contact = mongoose.model('Contact', {
+  name: String,
+  tel: String
+});
+app.use(express.json()); // to support JSON-encoded bodies
+// app.use(express.urlencoded());
 
+mongoose.connect('mongodb://localhost/phonebook')
+  .then(() => {
+    console.log('successfully connected to mongo db');
+  })
+  .catch((err) => {
+    console.log('failed connecting to mongo db', err);
+  });
+
+// REST API FOR DB
+app.get("/api/hello", hello);
+app.post("/api/phonebook", addPhonebook);
+app.delete("/api/phonebook/:phonebookId", delPhonebook);
+app.get("/api/phonebooks", getPhonebooks);
+app.get("/api/phonebook/:phonebook", getPhonebook);
+
+// SERVE APPLICATION FILES (HTML/CSS/JS/)
 app.use(express.static('Application'))
-
 app.listen(8080);
+
+function addPhonebook(req, res) {
+  const newContact = new Contact({
+    name: req.body.name,
+    tel: req.body.tel
+  });
+  newContact.save().then(() => console.log('contact created with info', newContact, req.body.name, req.body.tel));
+  res.status(200).send('Added new Contact');
+}
+
+function delPhonebook(req, res) {
+  Contact.deleteOne({
+    id: req.params.phonebookId
+  }, (err) => {
+    console.log('couldn\'t delete element', req.params.phonebookId);
+    res.status(500).send('Error at Deletion: ');
+    // return;
+  })
+  // console.log('deleted element:', req.params.phonebookId);
+  // res.status(200).send('deleted Contact');
+}
+
+function getPhonebooks(req, res) {
+  let data = Contact.find((err, dbres) => {
+    if (err)
+      console.error('error fetching data');
+    res.status(200).send(dbres);
+  });
+}
+
+function getPhonebook(req, res) {
+
+}
+
+function isValidContact(contactObj) {
+
+}
+
+function hello(req, res) {
+  res.status(200).send('World!');
+}
