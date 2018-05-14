da27efa473c85028358f6b9de42a6417aeeb9dc3
Romain Lussier <romain.lussier@external.engie.com>
2018-05-14 08:12:11 +0200

    Edit & tests remaining

--- Application/js/phonebookController.js
+++ Application/js/phonebookController.js
@@ -15,23 +15,33 @@ myAppModule.controller('phonebookController', function ($scope, $http, $location
       .error(function (error) {
         console.log("error Fetching Data : " + error);
       });
-    setTimeout($scope.fetchData, 200);
+    // setTimeout($scope.fetchData, 20000);
   }
 
-  $scope.addContact = function(newName,newTel){
-    $scope.data.push({name:newName,tel:newTel});
-    $http.post("/api/phonebook",JSON.stringify({name:newName,tel:newTel}),{headers:{'content-type':'application/json'}})
+  $scope.addContact = function (newName, newTel) {
+    $scope.data.push({
+      name: newName,
+      tel: newTel
+    });
+    $http.post("/api/phonebook", JSON.stringify({
+        name: newName,
+        tel: newTel
+      }), {
+        headers: {
+          'content-type': 'application/json'
+        }
+      })
       .success(function (response) {
         console.log('successfully added new Contact');
       })
       .error(function (error) {
         console.log("error adding new Contact : " + error);
       });
+      $scope.fetchData()
   }
-  
-  $scope.deleteContact = function(contactId){
+
+  $scope.deleteContact = function (contactId) {
     console.log('inside deleteContact');
-    console.log('Contacts:', $scope.data);
     $http.delete("/api/phonebook/" + contactId)
       .success(function (response) {
         console.log('successfully removed Contact: ', contactId);
@@ -39,6 +49,39 @@ myAppModule.controller('phonebookController', function ($scope, $http, $location
       .error(function (error) {
         console.log("error adding new Contact : " + error);
       });
+      $scope.fetchData()
+  }
+
+  $scope.editContact = function (contact) {
+    console.log('inside deleteContact');
+    // $http.post("/api/phonebook/" + contactId)
+    //   .success(function (response) {
+    //     console.log('successfully removed Contact: ', contactId);
+    //   })
+    //   .error(function (error) {
+    //     console.log("error adding new Contact : " + error);
+    //   });
+    $scope.fetchData()
+  }
+
+  $scope.validateForm = function (name, tel) {
+    if (name === undefined || tel === undefined) return false;
+    if (name.length <= 0) return false;
+    if (tel.length <= 0) return false;
+    let numberValidator = /\+[0-9]{1,4}\ [0-9]{2}\ [0-9]{6,}/;
+    if (!numberValidator.exec(tel)) return false;
+    // if (numberValidator.exec(tel)[0].length) return false;
+    return true;
+  }
+
+  $scope.enterEdition = function(contactId){
+    for(contact of $scope.data){
+      if(contact._id = contactId){
+        contact.edit = true;
+        console.log(contact);
+      }
+    }
+
   }
   init();
 });
--- expressserver.js
+++ expressserver.js
@@ -18,55 +18,87 @@ mongoose.connect('mongodb://localhost/phonebook')
   });
 
 // REST API FOR DB
-app.get("/api/hello", hello);
-app.post("/api/phonebook", addPhonebook);
-app.delete("/api/phonebook/:phonebookId", delPhonebook);
-app.get("/api/phonebooks", getPhonebooks);
-app.get("/api/phonebook/:phonebook", getPhonebook);
+app.get("/api/hello", sayHi);
+app.post("/api/phonebook", addContact);
+app.post("/api/phonebook/:contactId", editContact);
+app.delete("/api/phonebook/:contactId", deleteContact);
+app.get("/api/phonebooks", getContacts);
+app.get("/api/phonebook/:contactId", getContact);
 
 // SERVE APPLICATION FILES (HTML/CSS/JS/)
 app.use(express.static('Application'))
 app.listen(8080);
 
-function addPhonebook(req, res) {
-  const newContact = new Contact({
-    name: req.body.name,
-    tel: req.body.tel
-  });
-  newContact.save().then(() => console.log('contact created with info', newContact, req.body.name, req.body.tel));
-  res.status(200).send('Added new Contact');
+function addContact(req, res) {
+  if (isValidContact(req.body.name, req.body.tel)) {
+    const newContact = new Contact({
+      name: req.body.name,
+      tel: req.body.tel
+    });
+    newContact.save().then(() => console.log('contact created with info', newContact));
+    res.status(200).send('Added new Contact');
+  } else {
+    res.status(200).send('Invalid Contact Information provided');
+  }
 }
 
-function delPhonebook(req, res) {
+function deleteContact(req, res) {
   Contact.deleteOne({
-    _id: req.params.phonebookId
+    _id: req.params.contactId
   }, (err) => {
     if (err) {
-      console.log('couldn\'t delete element', req.params.phonebookId);
+      console.log('couldn\'t delete element', req.params.contactId);
       res.status(500).send('Error at Deletion: ');
     } else {
-      console.log('deleted element:', req.params.phonebookId);
+      console.log('deleted element:', req.params.contactId);
       res.status(200).send('deleted Contact');
     }
   })
 }
 
-function getPhonebooks(req, res) {
-  let data = Contact.find((err, dbres) => {
+function getContacts(req, res) {
+  Contact.find((err, dbres) => {
     if (err)
       console.error('error fetching data');
     res.status(200).send(dbres);
   });
 }
 
-function getPhonebook(req, res) {
-
+function editContact(req, res) {
+  if (isValidContact(req.body.name, req.body.tel)) {
+    Contact.findByIdAndUpdate(
+      req.params.contactId, {
+        name: req.body.name,
+        tel: req.body.tel
+      },
+      (err, dbres) => {
+        if (err)
+          console.error('error updating data', err);
+        res.status(200).send(dbres);
+      });
+  } else {
+    console.error('invalid data');
+    res.status(410).send('invalid data');
+  }
 }
 
-function isValidContact(contactObj) {
-
+function getContact(req, res) {
+  Contact.findById(req.params.contactId, (err, dbres) => {
+    if (err)
+      console.error('error fetching Contact', err);
+    res.status(200).send(dbres);
+  });
 }
 
-function hello(req, res) {
+function isValidContact(name, tel) {
+  if (name === undefined || tel === undefined) return false;
+  if (name.length <= 0) return false;
+  if (tel.length <= 0) return false;
+  const numberValidator = /\+[0-9]{1,4}\ [0-9]{2}\ [0-9]{6,}/;
+  if (!numberValidator.exec(tel)) return false;
+  return true;
+}
+
+function sayHi(req, res) {
   res.status(200).send('World!');
 }
